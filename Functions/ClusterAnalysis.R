c68527896762624f86406742140c6662860c0a71
Oceane Cassan <oceane.cassan@supagro.fr>
2020-03-05 18:47:20 +0100

    ranking clusters nice

--- Functions/ClusterAnalysis.R
+++ Functions/ClusterAnalysis.R
@@ -13,6 +13,16 @@ load("./Data/NitrateGenes.RData")
 # genes <- unique(unlist(DEGs))
 # cluster <- clustering(genes, data, nb_clusters = 7:14, norm = "TMM")
 
+translate <- function(text){
+  res = ""
+  if(grepl("c", text)){res = paste0(res, "AmbientCO2, ")}
+  else{res = paste0(res, "ElevatedCO2, ")}
+  if(grepl("N", text)){res = paste0(res, "Hight Nitrate, ")}
+  else{res = paste0(res, "Low Nitrate, ")}
+  if(grepl("f", text)){res = paste0(res, "Iron Starvation")}
+  else{res = paste0(res, "Iron Supply")}
+  return(res)
+}
 
 plotProfile <- function(cluster, k="none"){
   # plot all the profiles or the profile of cluster k
@@ -24,18 +34,20 @@ plotProfile <- function(cluster, k="none"){
   d$group <- str_split_fixed(d$variable, '_', 2)[,1]
   d$cluster <- clusters[match(d$gene, names(clusters))]
   if(k=="none"){
-     g <- ggplot(data = d, aes(x=group, y=value)) + geom_violin(alpha=0.7, aes( color = group), fill = "grey")  + geom_jitter(width = 0.1, alpha=0.01) +facet_wrap(~cluster) +
-      ylim(0, 0.3) + scale_colour_discrete("Condition")
+     g <- ggplot(data = d, aes(x=group, y=value)) + geom_boxplot(alpha=0.7, aes( color = group), fill = "grey", outlier.color = "black",outlier.alpha =0.1)  + geom_jitter(width = 0.1, alpha=0.0) +facet_wrap(~cluster, nrow=4) 
   }
   else{
-    g <- ggplot(data = d[d$cluster==k,], aes(x=group, y=value)) + geom_violin(alpha=0.7, aes(color = group), fill = "grey")  + geom_jitter(width = 0.1, alpha=0.02) +
-      scale_colour_discrete("Condition") 
+    g <- ggplot(data = d[d$cluster==k,], aes(x=group, y=value)) + geom_boxplot(outlier.alpha =0.2, outlier.color = "black", alpha=0.7, aes(color = group), fill = "grey")  + geom_jitter(width = 0.1, alpha=0.0) 
   }
   g +theme(plot.title = element_text(size=22, face="bold"),strip.text.x = element_text(size = 20),
            legend.title = element_text(size = 25, face="bold"), legend.text = element_text(size=20),
            axis.text.y = element_text(size = 18, angle = 30), axis.text.x = element_text(size = 0, hjust = 0, colour = "grey50"),
-           axis.title=element_text(size=17))
+           axis.title=element_text(size=24)) + xlab("") + ylab("Normalized expression") + scale_colour_discrete("Condition", labels=sapply(levels(as.factor(d$group)),translate)) +
+    stat_summary(fun.y=median, geom="line", aes(group=1), alpha=0.1, size = 1.5) +
+    ylim(0, 0.25) 
 }
+
+plotProfile(cluster, 4) + ggtitle("Clusters expression profiles")
 
 findNitrateGenes <- function(cluster, k="none"){
   if(k=="none"){
